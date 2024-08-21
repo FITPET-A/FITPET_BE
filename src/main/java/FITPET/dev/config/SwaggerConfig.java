6215242b5880cb2b8b07815be2149b8eea1bc3c5
cowboysj <su10jin11@khu.ac.kr>
2024-08-21 21:58:28 +0900

    [Docs] : 스웨거 설정

---
+++ src/main/java/FITPET/dev/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package FITPET.dev.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+    @OpenAPIDefinition(
+            info = @Info(title="FITPET",
+                    description = "큐시즘 Pit a Pat팀 FITPET 기업 프로젝트",
+                    version = "v1")
+    )
+    @Configuration
+    public class SwaggerConfig {
+        @Bean
+        public OpenAPI OpenAPI() {
+            return new OpenAPI()
+                    .components(new Components());
+        }
+
+    }
+
+
