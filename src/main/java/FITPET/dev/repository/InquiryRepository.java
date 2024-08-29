b0c6029adb400696ef107d988d5a52a4458be919
daeun084 <030804jk@naver.com>
2024-08-29 18:52:02 +0900

    chore: 1:1 문의 내역 조회 날짜 범위 변경

--- src/main/java/FITPET/dev/repository/InquiryRepository.java
+++ src/main/java/FITPET/dev/repository/InquiryRepository.java
@@ -4,6 +4,7 @@ import FITPET.dev.common.enums.InquiryStatus;
 import FITPET.dev.entity.Inquiry;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,7 +15,8 @@ public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
             "WHERE (:startDate IS NULL OR i.createdAt >= :startDate) " +
             "AND (:endDate IS NULL OR i.createdAt <= :endDate) " +
             "ORDER BY i.createdAt DESC ")
-    List<Inquiry> findByCreatedAtBetween(LocalDateTime startDate, LocalDateTime endDate);
+    List<Inquiry> findByCreatedAtBetween(@Param(value = "startDate") LocalDateTime startDate,
+                                         @Param(value = "endDate") LocalDateTime endDate);
 
 
     @Query("SELECT i FROM Inquiry i " +
@@ -22,5 +24,7 @@ public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
             "AND (:endDate IS NULL OR i.createdAt <= :endDate) " +
             "AND (:status IS NULL OR i.status = :status) " +
             "ORDER BY i.createdAt DESC ")
-    List<Inquiry> findByCreatedAtBetweenAndStatus(LocalDateTime startDate, LocalDateTime endDate, InquiryStatus status);
+    List<Inquiry> findByCreatedAtBetweenAndStatus(@Param(value = "startDate") LocalDateTime startDate,
+                                                  @Param(value = "endDate") LocalDateTime endDate,
+                                                  @Param(value = "status") InquiryStatus status);
 }
