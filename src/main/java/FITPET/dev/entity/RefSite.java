11fa57d6b97a90ad3dbdefe077315235b788b96e
cowboysj <su10jin11@khu.ac.kr>
2024-08-31 09:35:30 +0900

    feat : 유입 사이트 엔티티 설정

---
+++ src/main/java/FITPET/dev/entity/RefSite.java
@@ -0,0 +1,46 @@
+package FITPET.dev.entity;
+
+import FITPET.dev.common.base.BaseEntity;
+import com.fasterxml.jackson.databind.ser.Serializers.Base;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefSite extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long refSiteId;
+
+    @Column(nullable = true, length = 30)
+    private String channel;
+
+    @Column(nullable = false, length = 100)
+    private String url;
+
+    @Column(nullable = false, length = 30)
+    private String channelKor;
+
+    @Column(nullable = true)
+    private LocalDateTime deletedAt;
+
+
+
+
+
+
+
+}
