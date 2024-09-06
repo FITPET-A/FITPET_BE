2b8c1531de45483987b7ca947f9cfe1208c8c128
cowboysj <su10jin11@khu.ac.kr>
2024-09-06 15:50:50 +0900

    chore : 스웨거 bearer 토큰 설정 추가

--- src/main/java/FITPET/dev/common/config/SwaggerConfig.java
+++ src/main/java/FITPET/dev/common/config/SwaggerConfig.java
@@ -4,22 +4,35 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-    @OpenAPIDefinition(
-            info = @Info(title="FITPET",
-                    description = "큐시즘 Pit a Pat팀 FITPET 기업 프로젝트",
-                    version = "v1")
-    )
-    @Configuration
-    public class SwaggerConfig {
-        @Bean
-        public OpenAPI OpenAPI() {
-            return new OpenAPI()
-                    .components(new Components());
-        }
+@OpenAPIDefinition(
+        info = @Info(title="FITPET",
+                description = "큐시즘 Pit a Pat팀 FITPET 기업 프로젝트",
+                version = "v1")
+)
+@Configuration
+public class SwaggerConfig {
 
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        Components components = new Components()
+                .addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")
+                );
+
+        return new OpenAPI()
+                .components(components)
+                .addSecurityItem(securityRequirement);
     }
-
-
+}
