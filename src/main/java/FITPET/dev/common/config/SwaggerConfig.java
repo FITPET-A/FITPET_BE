d9bfa05d7235fca1e66ed176a55d4aef45dea14f
cowboysj <su10jin11@khu.ac.kr>
2024-09-06 19:48:12 +0900

    !hotfix : swagger serverUrl 수정

--- src/main/java/FITPET/dev/common/config/SwaggerConfig.java
+++ src/main/java/FITPET/dev/common/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package FITPET.dev.common.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.Servers;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
@@ -10,10 +12,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
-        info = @Info(title="FITPET",
+        info = @Info(title = "FITPET",
                 description = "큐시즘 Pit a Pat팀 FITPET 기업 프로젝트",
-                version = "v1")
+                version = "v1"),
+        servers = {
+                @Server(url = "https://3.38.244.36.nip.io",
+                        description = "서버 URL"),
+        }
 )
+
 @Configuration
 public class SwaggerConfig {
 
