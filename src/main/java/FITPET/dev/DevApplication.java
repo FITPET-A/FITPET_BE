8263b7e0f8aa46cd75d64f59ac27ffd1fe22b8ed
daeun084 <030804jk@naver.com>
2024-09-07 17:22:10 +0900

    fix: set timeZone to Seoul

--- src/main/java/FITPET/dev/DevApplication.java
+++ src/main/java/FITPET/dev/DevApplication.java
@@ -1,8 +1,11 @@
 package FITPET.dev;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,6 +13,11 @@ public class DevApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevApplication.class, args);
+	}
+
+	@PostConstruct
+	public void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
