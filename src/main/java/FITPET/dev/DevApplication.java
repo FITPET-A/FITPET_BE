39e49d4a5d4d1bc2590d30e9baa968207abaf06a
Daeun <122000839+daeun084@users.noreply.github.com>
2024-09-20 00:39:26 +0900

    !hotfix: TimeZone 설정

--- src/main/java/FITPET/dev/DevApplication.java
+++ src/main/java/FITPET/dev/DevApplication.java
@@ -3,6 +3,8 @@ package FITPET.dev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import java.util.TimeZone;
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,6 +12,11 @@ public class DevApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevApplication.class, args);
+	}
+
+	@PostConstruct
+	public void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
